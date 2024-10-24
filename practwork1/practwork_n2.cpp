3d375d2b58e06fa9a3b1f9b935f203cad9970b21
lostosha <107139830+lostosha@users.noreply.github.com>
2024-10-24 20:59:48 +0700

    Add files via upload
    задание 2

---
+++ practwork1/practwork_n2.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	cout << "Enter number: ";
+	int A;
+	cin >> A;
+	cout << "That`s the number you entered: " << A << endl;
+	return 0;
+}

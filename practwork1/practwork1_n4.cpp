92b96b1c291e93b84be1927ddb12ab2d7fee760c
lostosha <107139830+lostosha@users.noreply.github.com>
2024-10-24 21:09:43 +0700

    Add files via upload
    задание 4

---
+++ practwork1/practwork1_n4.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	cout << "Enter R: ";
+	int R;
+	cin >> R;
+	cout << "The diameter is " << (R * 2) << endl;
+
+	return 0;
+}
+

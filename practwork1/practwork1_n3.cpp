b9c3e100fb68137f2e51871741e2fc7a2fbf8a6d
lostosha <107139830+lostosha@users.noreply.github.com>
2024-10-24 21:05:24 +0700

    Add files via upload
    задание 3

---
+++ practwork1/practwork1_n3.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	cout << "Enter M, N: ";
+	int M, N;
+	cin >> M >> N;
+	if (M > N) cout << "Number m > n" << endl;
+	else if (M < N) cout << "Number m < n" << endl;
+	else cout << "The numbers are equal" << endl;
+
+	return 0;
+}

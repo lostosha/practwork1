e004f8e2d8f78e11f0d60378cd1d529ac7e7a073
admin <admin@DESKTOP-RCD43QG>
2024-10-24 20:34:18 +0700

    Добавьте файлы проекта.

---
+++ practwork1/practwork1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	cout << "Введите A и B: ";
+	int A, B;
+	cin >> A >> B;
+	if (A != 0 && B % A == 0) {
+		cout << "A - делитель B";
+	}
+	else {
+		cout << "A - не делитель B";
+	}
+	return 0;
+}
+
+
